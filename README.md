983d41cebefae18fa4f8d5f62a0d4795aa41b908
陈新仁 <chenxinren@qudian.com>
2019-06-05 21:30:21 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# laravel-redislock
+# 简易好用的redis分布式锁
+> 支持原子上锁，原子解锁，同端保证
+
+### 引入 composer:
+
+```
+    "repositories": [
+        {
+            "type": "git",
+            "url": "git@git.qufenqi.com:chenxinren/laravel-redislock.git"
+        }
+    }
+```
+
+```
+ composer require "cxr/laravel-redislock:v0.0.1"
+```
+
+### 如何使用
+>laravel 需开启 Redis Facades
+```php
+    DistributedLock::lock('key', 3, 'default'); //上锁3s，指定连接串default
+    
+    DistributedLock::unlock('key', 'default');  //解锁，指定连接串default
+ 
+```
---
+++ src/RedisLock/DistributedLock.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * 分布式上锁工具类
+ * User: cxr
+ * Date: 2019-06-05
+ * Time: 17:24
+ */
+
+namespace RedisLock\DistributedLock;
+
+use Illuminate\Support\Facades\Redis;
+
+class DistributedLock
+{
+    private static $uniqid = '1'; //未防止异端删key增加请求唯一标示
+
+    /**
+     * 上锁
+     * 保证原子性写操作
+     * @param String $key 上锁key
+     * @param int $lockExpire 过期时间
+     * @param string $connection 可指定链接串
+     * @return bool
+     */
+    public static function lock($key, $lockExpire = 3, $connection = 'default')
+    {
+        self::$requestId = uniqid();
+        $result = Redis::connection($connection)->set($key, self::$uniqid, 'EX', $lockExpire, 'NX');
+        return !is_null($result);
+    }
+
+    /**
+     * 解锁
+     * 采用eval保证同端删除操作，保证原子性操作
+     * @param $key
+     * @param string $connection
+     * @return bool
+     */
+    public static function unlock($key, $connection = 'default')
+    {
+        $luaScript = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+        $result = Redis::connection($connection)->eval($luaScript, 1, $key, self::$uniqid);
+        return (bool)$result;
+    }
+}
